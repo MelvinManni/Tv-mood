01a1a23e6a292007b6582e47381cdcd41b63891c
MelvinManni <melvinnnamani@gmail.com>
2020-06-02 23:07:12 +0100

    Updates

---
+++ precache-manifest.752bb3d02ef00e897bde018621187370.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231bfd08762308d74040a449f3aa71c1",
+    "url": "/MelvinManni/Tv-mood/index.html"
+  },
+  {
+    "revision": "6f16a39655e6de7800b3",
+    "url": "/MelvinManni/Tv-mood/static/css/main.434134a6.chunk.css"
+  },
+  {
+    "revision": "dc98c0ced1a63dd8de80",
+    "url": "/MelvinManni/Tv-mood/static/js/2.a3dd6422.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/MelvinManni/Tv-mood/static/js/2.a3dd6422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f16a39655e6de7800b3",
+    "url": "/MelvinManni/Tv-mood/static/js/main.dce7250c.chunk.js"
+  },
+  {
+    "revision": "f59a5b4bc22e1a25036c",
+    "url": "/MelvinManni/Tv-mood/static/js/runtime-main.d1542309.js"
+  },
+  {
+    "revision": "bdd50816692913de0ff6967245853f1e",
+    "url": "/MelvinManni/Tv-mood/static/media/error.bdd50816.svg"
+  },
+  {
+    "revision": "700dba7024b41484270b6d1dc85d2f7d",
+    "url": "/MelvinManni/Tv-mood/static/media/facebook.700dba70.svg"
+  },
+  {
+    "revision": "3d12618867f22f459e8c74f6dbaf6155",
+    "url": "/MelvinManni/Tv-mood/static/media/film-reel.3d126188.svg"
+  },
+  {
+    "revision": "c4cb17e1b6783ff8a21367ad15365a82",
+    "url": "/MelvinManni/Tv-mood/static/media/gmail.c4cb17e1.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/MelvinManni/Tv-mood/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "a21e88ca8d10cf8c98fea58566a3ac69",
+    "url": "/MelvinManni/Tv-mood/static/media/movie.a21e88ca.gif"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/MelvinManni/Tv-mood/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "4e91b3ac14542ef4b5c556501329ac47",
+    "url": "/MelvinManni/Tv-mood/static/media/television.4e91b3ac.svg"
+  },
+  {
+    "revision": "b9d31ddaba3012eaf7afd436f56826a8",
+    "url": "/MelvinManni/Tv-mood/static/media/television.b9d31dda.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/MelvinManni/Tv-mood/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "ad8f201bfd0779972f9ea06f00920d99",
+    "url": "/MelvinManni/Tv-mood/static/media/youtube.ad8f201b.svg"
+  }
+]);
