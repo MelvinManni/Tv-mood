cb1a7df90066b2345b71b751884009d4faeaaf52
MelvinManni <melvinnnamani@gmail.com>
2020-06-03 13:09:39 +0100

    ++404 page

--- src/pages/404/distil/index.jsx
+++ src/pages/404/distil/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import err from './../assets/2bf9f053182371.592b9aebf2af0.jpg'
+import React from 'react';
+import err from './../assets/2bf9f053182371.592b9aebf2af0.jpg';
 
 function ErrorPage() {
-    return (
-        <div>
-            <img src={err} width='100%' height='420px' alt=" Error 404"/>
-        </div>
-    )
+  return (
+    <div>
+      <img src={err} width='100%' alt=' Error 404' />
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
