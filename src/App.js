137e76eb9347cfaba4b0bfec05538827fb21a0ed
MelvinManni <melvinnnamani@gmail.com>
2020-06-03 12:57:53 +0100

    ++404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router';
 import Home from './pages/home/distil';
 import Series from './pages/series/distil';
 import Movies from './pages/movies/distil';
+import ErrorPage from './pages/404/distil';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/series' component={Series} />
           <Route exact path='/movies' component={Movies} />
+          <Route path='*' component={ErrorPage} />
         </Switch>
       </HeaderFooter>
     </div>
