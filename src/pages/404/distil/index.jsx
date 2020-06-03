ca5fc38bedd2210925aede2e39208ff7000aede3
MelvinManni <melvinnnamani@gmail.com>
2020-06-03 13:31:49 +0100

    404 image sizing

--- src/pages/404/distil/index.jsx
+++ src/pages/404/distil/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import err from './../assets/2bf9f053182371.592b9aebf2af0.jpg';
 
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const imgStyle = {
+  maxWidth: 400,
+  minWidth: 280
+};
+
 function ErrorPage() {
   return (
-    <div>
-      <img src={err} width='100%' alt=' Error 404' />
+    <div style={style}>
+      <img src={err} style={imgStyle} alt=' Error 404' />
     </div>
   );
 }
