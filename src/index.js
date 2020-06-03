26fd3b194d7d4ac70111433bba6727e7f46323d6
MelvinManni <melvinnnamani@gmail.com>
2020-06-03 11:20:19 +0100

    Setting up for netlify ++netlify.toml

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
